4c7d360545b814fe3fcb0ac024e0fbcfd3e28f7d
Seungmin Jeong <sontneptm2@gmail.com>
2022-03-30 18:54:55 +0900

    update

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,7 +1,10 @@
+import Button from "./Button";
+
 function App() {
   return (
     <div>
-      <h1>welcome back!</h1>
+      <h1>welcome back!!!</h1>
+      <Button text={"continue"} />
     </div>
   );
 }
---
+++ react-for-beginners/src/Button.js
@@ -0,0 +1,11 @@
+import propTypes from "prop-types";
+
+function Button({ text }) {
+  return <button>{text}</button>;
+}
+
+Button.propTypes = {
+  text: propTypes.string.isRequired,
+};
+
+export default Button;
