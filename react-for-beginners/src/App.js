94b03b0c9aba7c253b2ffa6e0ca877f8e4bd9e85
Seungmin Jeong <sontneptm2@gmail.com>
2022-03-31 19:31:08 +0900

    update

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>I'm ready</h1>
+      <h1>The coins! {coins.length}</h1>
+      {loading ? <strong>loading....</strong> : null}
+      <ul>
+        {coins.map((item) => (
+          <li key={item.id}>
+            {item.name} ({item.symbol}) : ${item.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
