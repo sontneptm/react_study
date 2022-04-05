e835f02c249792dd2d2390c8656b90d445a80670
Seungmin Jeong <sontneptm2@gmail.com>
2022-04-05 12:46:25 +0900

    I'm done

--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -1,19 +1,24 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
 function Movie({ id, cover_img, title, summary, genres }) {
   return (
-    <div>
+    <div className={styles.movie}>
       <img src={cover_img} alt={title} />
-      <h2>
-        <Link to={"./movie/" + id}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((genre) => (
-          <li key={genre}> {genre}</li>
-        ))}
-      </ul>
+      <div className={styles.movie__title}>
+        <h2>
+          <Link to={"./movie/" + id}>{title}</Link>
+        </h2>
+        <p className={styles.movie__year}>
+          {summary.length > 235 ? summary.substring(0, 235) + "..." : summary}
+        </p>
+        <ul className={styles.movie__genres}>
+          {genres.map((genre) => (
+            <li key={genre}> {genre}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- react-for-beginners/src/routes/Home.js
+++ react-for-beginners/src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -19,11 +20,13 @@ function Home() {
   }, []);
 
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1> Loading... </h1>
+        <div className={styles.loader}>
+          <h1> Loading... </h1>
+        </div>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
